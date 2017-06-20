944b11edfcf3655385354954507cad26115f370b
isball <lililiqiu@qq.com>
2017-06-20 16:29:57 +0800

    第一次push

---
+++ src/test/TestOne.java
@@ -0,0 +1,8 @@
+package test;
+
+public class TestOne {
+	public String s = "one";
+	public void out(){
+		System.out.println(s);
+	}
+}
---
+++ src/test/TestTwo.java
@@ -0,0 +1,7 @@
+package test;
+
+public class TestTwo {
+	public static void main(String[] args) {
+		new TestOne().out();
+	}
+}
